f0b48d516ba41d4915f7bd6e0f949ecc742fc8fd
Sahil-Kadam-2003 <sahilkadam2205@gmail.com>
2024-08-30 18:13:42 +0530

    aws word remove

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const app = express();
 app.use(express.json());
 
 app.get('/hello', (req, res) => {
-  res.send({"message":"AWS Hello Lambda!","status code":"200"});
+  res.send({"message":"Hello Lambda!","status code":"200"});
 });
 
 app.get('/hello-One', (req, res) => {
-  res.send({"Message":"AWS Hello-One Lambda!","status code":"200"});
+  res.send({"Message":"Hello-One Lambda!","status code":"200"});
 });
 
 app.get('/hello-Two', (req, res) => {
-  res.send({"message":"AWS Hello-Two Lambda!","status code":"200"});
+  res.send({"message":"Hello-Two Lambda!","status code":"200"});
 });
 
 module.exports.handler = serverless(app);
