1172d7d0dee58fb0aca32786faf1f2a95709dc85
Sahil-Kadam-2003 <sahilkadam2205@gmail.com>
2024-08-30 16:56:25 +0530

    method change

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from api!' }),
-  };
-};
+// index.js
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/hello', (req, res) => {
+  res.send('AWS Hello Lambda!');
+});
+
+app.get('/hello-One', (req, res) => {
+  res.send('AWS Hello-One Lambda!');
+});
+
+app.get('/hello-Two', (req, res) => {
+  res.send('AWS Hello-Two Lambda!');
+});
+
+module.exports.handler = serverless(app);
